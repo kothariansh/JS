aa6a11a75a7a7a0b7cb45311d7ebb6243608700f
Ansh <anshkothari558@gmail.com>
2021-06-23 16:04:01 +0530

    initial practice

--- first_1.js
+++ first_1.js
@@ -1,3 +1,13 @@
-<HTML> ]
-    
-</HTML>
+var message = "in global"; 
+console.log("global: message = " + message);
+
+ var a = function() {
+  var message = "inside a"; 
+ console.log("a: message = " + message);
+ b();
+}
+
+function b () {
+    console.log("b: message = " + message);
+}
+a();
